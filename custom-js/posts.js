28e352efd291e0c10b6acc55d347ce14dd31e01f
AleksBobic <bobicaleks@yahoo.com>
2020-03-05 20:12:33 +0100

    start on blog.html + refactor bmi.html

--- custom-js/posts.js
+++ custom-js/posts.js
@@ -12,9 +12,45 @@ let posts = [
 		"created_at" : "February 17, 2019"
 	},
 	{
-		"title" : "10 States At Risk of Rural Hospital Closures",
+		"title" : "Man gets life in prison for stabbing",
+		"tag" : "#Body",
+		"img_path" : "img/blog/blog-3.jpg",
+		"created_at" : "February 17, 2019"
+	},
+	{
+		"title" : "Russia's first male synchronised swimmer",
+		"tag" : "#Running",
+		"img_path" : "img/blog/blog-4.jpg",
+		"created_at" : "February 17, 2019"
+	},
+	{
+		"title" : "NYC measles vaccination order prevails in court",
 		"tag" : "#Gym",
-		"img_path" : "img/blog/blog-1.jpg",
+		"img_path" : "img/blog/blog-5.jpg",
+		"created_at" : "February 17, 2019"
+	},
+	{
+		"title" : "The Week in Pictures: April 11 - 18",
+		"tag" : "#Sport",
+		"img_path" : "img/blog/blog-6.jpg",
+		"created_at" : "February 17, 2019"
+	},
+	{
+		"title" : "Man who pretended to be faces new charges",
+		"tag" : "#Gym",
+		"img_path" : "img/blog/blog-7.jpg",
+		"created_at" : "February 17, 2019"
+	},
+	{
+		"title" : "10 States At Risk of Rural Hospital Closures",
+		"tag" : "#Body",
+		"img_path" : "img/blog/blog-8.jpg",
+		"created_at" : "February 17, 2019"
+	},
+	{
+		"title" : "Lenny Kravitz gives Arch Digest a look inside",
+		"tag" : "#Gym",
+		"img_path" : "img/blog/blog-9.jpg",
 		"created_at" : "February 17, 2019"
 	},
 ];
@@ -42,12 +78,15 @@ $('#list-posts').html(postsTemplate);
 
 function filterPosts() {
 	let selectTag = "#" + $('#posts').find(":selected").text();
-
+	console.log(selectTag)
+	
+	if (selectTag.includes("--Select--")) {
+		$('#list-posts').html(postsTemplate);
+	}
 	var selectedPosts = [];
 
 	for (var i = 0; i < posts.length; i++) {
 		var post = posts[i];
-		console.log(selectTag, post.tag);
 		if (post.tag == selectTag) {
 			selectedPosts.push(post);
 		}
